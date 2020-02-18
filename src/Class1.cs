860e9e7676131aee0bc56615e5c0405f6c7dcae5
Eivind Gussiås Løkseth <eloekset@msn.com>
2020-02-18 20:24:18 +0100

    Add dummy C# classlib

---
+++ src/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzurePipelinesTest
+{
+    public class Class1
+    {
+        public override string ToString()
+        {
+            return "The most interesting class lib ever";
+        }
+    }
+}
